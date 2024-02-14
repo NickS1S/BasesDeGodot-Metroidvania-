61bf04f95c628740c0a2d0596e6dc312ac0264bd
NickS1S <nicolasmaximiliano020202@gmail.com>
2024-02-13 21:45:31 -0300

    Modificacion de enemigo

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,43 +1,44 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 90
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var motion := Vector2()
-
 var player = null
+
 func _ready():
-	motion.x = SPEED
+	velocity.x = -SPEED
+	$AnimatedSprite2D.play("Walk")
 	pass 
 
 func _next_to_left_wall() -> bool:
+	print("Colisione izquierda")
 	return $LeftR.is_colliding()
 
 func _next_to_right_wall() -> bool:
+	print("Colisione derecha")
 	return $RightR.is_colliding()
 
 func _floor_detection() -> bool:
+	print("Colisione caida")
 	return $AnimatedSprite2D/FloorDetection.is_colliding()
 
-func _flip():
+func flip():
 	if _next_to_left_wall() or _next_to_right_wall() or !_floor_detection():
-		motion.x *= -1
+		velocity.x *= SPEED
 		$AnimatedSprite2D.scale.x *= -1
 
 
+@warning_ignore("unused_parameter")
 func _process(delta):
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	
-	if player != null:
-		motion = position.direction_to(player.position)
-	else:
-		move_and_slide()
-	_flip()
+	velocity.x = -SPEED
+	flip()
+	move_and_slide()
+	print("Posición del enemigo: ", position)
 	pass
 
-
+func follow():
+	if player != null:
+		velocity = position.direction_to(player.position)*SPEED
+		
 func _on_area_2d_body_entered(body):
 	if body != self:
 		player = body
